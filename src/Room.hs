2f4463b257242a82c53b8f4fff47b97783111eb6
ToF- <cthibaut@octo.com>
2019-12-03 20:35:26 +0100

    room evolves in temperature

--- src/Room.hs
+++ src/Room.hs
@@ -1,5 +1,24 @@
 module Room where
 
-initialRoom = 15.0
+type Room = ((Temperature, Position),History)
+type Temperature = Double
+type Position = Integer
 
-temperature = id
+type History = [Temperature]
+
+initialRoom = ((15.0, 100),[])
+
+temperature = fst . fst
+position    = snd . fst
+
+update :: Room -> Room
+update ((t,p),h) = ((t + ((fromIntegral p) / 10.0 + 2.0 - temperatureAtTime 3 h) / 3.0, p), t:h)
+    where
+    temperatureAtTime n h | n < length h = h!!n
+                          | otherwise    = 15
+
+setPosition :: Room -> Position -> Room
+setPosition ((t,_),h) p = ((t,p),h)
+
+history :: Room -> History 
+history = snd
--- test/RoomSpec.hs
+++ test/RoomSpec.hs
@@ -4,5 +4,27 @@ import Test.Hspec
 import Room
 
 spec = describe "room" $ do
+    let apply n f i = (iterate f i) !! n
+
     it "should initially have a temperature of 15.0" $ do
         temperature initialRoom `shouldBe` 15.0
+
+    it "should have an initial position set to 100" $ do
+        position initialRoom `shouldBe` 100
+
+    it "should evolve in temperature after update" $ do
+        temperature (update initialRoom) `shouldBe` 14.0
+        temperature (apply 2 update initialRoom) `shouldBe` 13.0
+
+    it "should allow its position to be modified" $ do
+        position (setPosition initialRoom 50) `shouldBe` 50
+
+    it "should keep a history of updates" $ do
+        let h = history (apply 5 update initialRoom) 
+        length h  `shouldBe` 5
+        h!!4 `shouldBe` 15.0
+        h!!3  `shouldBe` 14.0
+
+    it "should have its temperature decreasing differently after 5 updates" $ do
+        temperature (apply 6 update initialRoom) `shouldBe` 9.333333333333334   
+
