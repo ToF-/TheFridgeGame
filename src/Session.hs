607648d01ab1b06bd2f4cf9d2965b9fd8e010b6a
Christophe Thibaut <cthibaut@octo.com>
2019-12-15 00:29:10 +0100

    testing interactive session with writer mock. stub input causes loopSession to indefinetely loop instead of quitting because the input is given entirely anew at each loop

--- src/Session.hs
+++ src/Session.hs
@@ -1,11 +1,14 @@
 module Session where
 import Data.Char
+import Room
 import Game
+import Simulation
 
 data Command = Quit
              | List
              | Help
-             | State String
+             | State PlayerId
+             | Pos PlayerId Position 
     deriving (Eq, Show, Read)
 
 command :: String -> Maybe Command
@@ -16,3 +19,20 @@ command s = case reads (capitalize s) of
 capitalize :: String -> String
 capitalize [] = []
 capitalize (c:s) = toUpper c : map toLower s
+
+prompt :: Monad m => (String -> m ()) -> m () 
+prompt out = out "Quit | List | Help | State \"id\" | Pos \"id\" n\n"
+
+entry :: Monad m => (m String) -> m (Maybe Command)
+entry imp = fmap command imp 
+
+sessionLoop :: Monad m => Game -> (m String) -> (String -> m ()) -> m ()
+sessionLoop g imp out = do
+    prompt out
+    cmd <- entry imp
+    case cmd of
+      Just Quit -> out "Bye"
+      Nothing -> do
+          out "???"
+          sessionLoop g imp out
+
--- test/SessionSpec.hs
+++ test/SessionSpec.hs
@@ -1,14 +1,45 @@
 module SessionSpec where
 
 import Test.Hspec
+import Control.Monad.Writer (writer, runWriter)
 import Session
+import Game
 
 spec = describe "session" $ do
-    it "should recognize a command" $ do
-        command "foo" `shouldBe` Nothing
-        command "Quit" `shouldBe` Just Quit
-        command "List" `shouldBe` Just List
-        command "Help" `shouldBe` Just Help
-        
-        command "QUIT" `shouldBe` Just Quit
-        command "state \"ToF\"" `shouldBe` Just (State "tof")
+    describe "command" $ do
+        it "should recognize a command" $ do
+            command "foo" `shouldBe` Nothing
+            command "Quit" `shouldBe` Just Quit
+            command "List" `shouldBe` Just List
+            command "Help" `shouldBe` Just Help
+            command "State \"ToF\"" `shouldBe` Just (State "tof")
+            command "Pos \"ToF\" 42" `shouldBe` Just (Pos "tof" 42) 
+    describe "prompt" $ do
+        it "should display a prompt" $ do
+            let out = \s -> writer ((), s)
+                run = prompt out
+            (snd (runWriter run)) `shouldBe` "Quit | List | Help | State \"id\" | Pos \"id\" n\n"
+
+    describe "entry" $ do
+        it "should read an entry and recognize a command" $ do
+            let imp = return "Pos \"tof\" 42\n"
+            cmd <- entry imp 
+            cmd `shouldBe` Just (Pos "tof" 42) 
+
+    describe "sessionLoop" $ do
+        it "should recognize commands entered and execute them until quitting" $ do
+            ["Quit"] `sessionShouldBe` ["Quit | List | Help | State \"id\" | Pos \"id\" n" ,"Bye"]
+
+        it "should signal an incorrect command" $ do
+            ["foo", "Quit"] `sessionShouldBe` ["Quit | List | Help | State \"id\" | Pos \"id\" n" 
+                                              ,"???"
+                                              ,"Quit | List | Help | State \"id\" | Pos \"id\" n"
+                                              ,"Bye"]
+
+
+sessionShouldBe ls expected = do
+        let imp = return (unlines ls)
+            out = \s -> writer ((), s)
+            run = sessionLoop newGame imp out
+        let result = (snd (runWriter run))
+        lines result `shouldBe` expected
