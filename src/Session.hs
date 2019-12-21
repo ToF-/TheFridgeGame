467261a43f4d28cbb1b81c1064aa24644a55c6cf
Christophe Thibaut <cthibaut@octo.com>
2019-12-21 09:01:02 +0100

    interpret commands so that initial of a command works

--- src/Session.hs
+++ src/Session.hs
@@ -9,48 +9,63 @@ data Command = Quit
              | List
              | Help
              | Add PlayerId
-             | State PlayerId
+             | Display PlayerId
              | Pos PlayerId Position 
-             | Go
-             | Halt
+             | Run
+             | End
              | Start PlayerId
-             | Stop PlayerId
+             | Break PlayerId
     deriving (Eq, Show, Read)
 
 type GameResult = (Game, [String])
 
 command :: String -> Maybe Command
-command s = case reads (normalize s) of
-              [(cmd,_)] -> Just cmd
-              [] -> Nothing
+command s = interpret (unwords (expandFirst (words s))) 
+
+interpret :: String -> Maybe Command
+interpret s = case reads (normalize s)  of
+                [(cmd,_)] -> Just cmd
+                [] -> Nothing
+
+expandFirst :: [String] -> [String]
+expandFirst [] = []
+expandFirst (w:ws) = expand w : ws
+
+expand :: String -> String
+expand s = case [k | 
+                 k <- keywords, 
+                 (normalize s) == take (length s) k] of
+    [k] -> k
+    [] -> s
+keywords = words "Quit List Help Add Display Pos Run End Start Break"
 
 normalize :: String -> String
 normalize = unwords . capitalizeFirst . words
-    where
-    capitalizeFirst :: [String] -> [String]
-    capitalizeFirst [] = []
-    capitalizeFirst (w:ws) = capitalize w : ws
+capitalizeFirst :: [String] -> [String]
+capitalizeFirst [] = []
+capitalizeFirst (w:ws) = capitalize w : ws
 
-    capitalize :: String -> String
-    capitalize [] = []
-    capitalize (c:s) = toUpper c : map toLower s
+capitalize :: String -> String
+capitalize [] = []
+capitalize (c:s) = toUpper c : map toLower s
 
 prompt :: Monad m => (String -> m ()) -> m () 
-prompt out = out "Quit | List | Go | Halt | Add \"id\" | Start \"id\" | Stop \"id\" | State \"id\" | Pos \"id\" n\n"
+prompt out = out "Quit | List | Run | End | Add \"id\" | Start \"id\" | Break \"id\" | Display \"id\" | Pos \"id\" n\n"
 
 entry :: Monad m => (m String) -> m (Maybe Command)
 entry imp = fmap command imp 
 
 doCommand :: Game -> (Maybe Command) -> GameResult
 doCommand g cmd = case cmd of
+                  Just Help -> (g, help)
                   Just Quit -> (g, ["Bye"])
                   Just List -> (g, showAll g)
                   Just (Add playerId) -> addNewPlayer playerId g
                   Just (Start playerId) -> startPlayer playerId g
-                  Just (Stop playerId) -> stopPlayer playerId g
-                  Just Go -> go g
-                  Just Halt -> halt g
-                  Just (State playerId) -> playerState playerId g
+                  Just (Break playerId) -> stopPlayer playerId g
+                  Just Run -> go g
+                  Just End -> halt g
+                  Just (Display playerId) -> playerDisplay playerId g
                   Just (Pos playerId n) -> playerSetPosition playerId n g
                   Nothing -> (g,["???"])
 
@@ -70,12 +85,12 @@ startPlayer playerId g = let g' = startForPlayer playerId g in
 stopPlayer :: PlayerId -> Game -> GameResult
 stopPlayer playerId g = let g' = stopForPlayer playerId g in
                              case stateForPlayer playerId g of
-                               Right _ -> (g',["Stopping simulation for "++show playerId])
+                               Right _ -> (g',["Pausing simulation for "++show playerId])
                                Left _ -> (g, ["Player " ++ show playerId ++ " is not in the game."])
 
-playerState :: PlayerId -> Game -> GameResult
-playerState playerId g = case stateForPlayer playerId g of
-                           Right (t,p) -> (g, ["State for " ++ show playerId ++ ": " ++ (show t) ++ " " ++ (show p)])
+playerDisplay :: PlayerId -> Game -> GameResult
+playerDisplay playerId g = case stateForPlayer playerId g of
+                           Right (t,p) -> (g, ["Display for " ++ show playerId ++ ": " ++ (show t) ++ " " ++ (show p)])
                            Left _ -> (g, ["Player " ++ show playerId ++ " is not in the game."])
 
 playerSetPosition :: PlayerId -> Position -> Game -> (Game,[String])
@@ -88,7 +103,7 @@ go :: Game -> GameResult
 go g = (startAll g, [ "Starting all simulations" ])
 
 halt :: Game -> GameResult
-halt g = (stopAll g, ["Stopping all simulations"])
+halt g = (stopAll g, ["Pausing all simulations"])
 
 pureGameLoop :: Monad m => Game -> (m String) -> (String -> m ()) -> m ()
 pureGameLoop g inp out = do
@@ -113,3 +128,15 @@ updateConcurrentGame mvar = do
     g <- takeMVar mvar
     let g' = updateRunningSimulations g
     putMVar mvar g'
+
+help :: [String]
+help = ["Help : display this message"
+       ,"Quit : quit the game"
+       ,"Add     [ABCDEF] : add a player in the game"
+       ,"Start   [ABCDEF] : start/resume simulation for a player"
+       ,"Pos     [ABCDEF] <0--100> : set position for a player"
+       ,"Display [ABCDEF] : display the state of a player"
+       ,"Break   [ABCDEF] : pause simulation for a player"
+       ,"Run : start/resume simulation for all players"
+       ,"End : pause simulation for all players"
+       ,"List : display the state of all players"]
--- test/SessionSpec.hs
+++ test/SessionSpec.hs
@@ -9,26 +9,46 @@ import Data.Function
 spec = describe "session" $ do
     describe "command" $ do
         it "should recognize a command" $ do
-            command "foo" `shouldBe` Nothing
-            command "Quit" `shouldBe` Just Quit
-            command "List" `shouldBe` Just List
-            command "Help" `shouldBe` Just Help
             command "Add A" `shouldBe` Just (Add A)
-            command "State A" `shouldBe` Just (State A)
+            command "Display A" `shouldBe` Just (Display A)
+            command "foo" `shouldBe` Nothing
+            command "Run"   `shouldBe` Just Run
+            command "End" `shouldBe` Just End
+            command "List" `shouldBe` Just List
             command "Pos A 42" `shouldBe` Just (Pos A 42) 
+            command "Quit" `shouldBe` Just Quit
             command "Start A" `shouldBe` Just (Start A) 
-            command "Stop A" `shouldBe` Just (Stop A) 
-            command "Halt" `shouldBe` Just Halt
-            command "Go"   `shouldBe` Just Go
+            command "Break A" `shouldBe` Just (Break A) 
+
+    describe "help" $ do
+        it "should display help" $ do
+            (doCommand newGame (Just Help)) `shouldBe` (newGame,
+                ["Help : display this message"
+                ,"Quit : quit the game"
+                ,"Add     [ABCDEF] : add a player in the game"
+                ,"Start   [ABCDEF] : start/resume simulation for a player"
+                ,"Pos     [ABCDEF] <0--100> : set position for a player"
+                ,"Display [ABCDEF] : display the state of a player"
+                ,"Break   [ABCDEF] : pause simulation for a player"
+                ,"Run : start/resume simulation for all players"
+                ,"End : pause simulation for all players"
+                ,"List : display the state of all players"])
+
 
     describe "prompt" $ do
         it "should display a prompt" $ do
             let out = \s -> writer ((), s)
                 run = prompt out
-            (snd (runWriter run)) `shouldBe` "Quit | List | Go | Halt | Add \"id\" | Start \"id\" | Stop \"id\" | State \"id\" | Pos \"id\" n\n"
+            (snd (runWriter run)) `shouldBe` "Quit | List | Run | End | Add \"id\" | Start \"id\" | Break \"id\" | Display \"id\" | Pos \"id\" n\n"
+
     describe "entry" $ do
         it "should read an entry and recognize a command" $ do
             let imp = return "Pos A 42\n"
+            cmd <- entry imp 
+            cmd `shouldBe` Just (Pos A 42) 
+
+        it "should recognize only a part of the keyword in a command" $ do
+            let imp = return "p A 42\n"
             cmd <- entry imp 
             cmd `shouldBe` Just (Pos A 42) 
 
@@ -43,14 +63,14 @@ spec = describe "session" $ do
         it "should add a player if not already added" $ do
             let (g',msg) = doCommand newGame $ Just $ Add A
             msg  `shouldBe` ["Player A added to the game."]
-            let (g'',msg) = doCommand g' $ Just $ State A
-            msg  `shouldBe` ["State for A: 15.0 100"]
+            let (g'',msg) = doCommand g' $ Just $ Display A
+            msg  `shouldBe` ["Display for A: 15.0 100"]
             let (g'',msg) = doCommand g' $ Just $ Add A
             msg  `shouldBe` ["Player A is already in the game."]
             g''  `shouldBe` g'
 
         it "should display an error if asked the state for a non player" $ do
-            let (g',msg) = doCommand newGame $ Just $ State A
+            let (g',msg) = doCommand newGame $ Just $ Display A
             msg  `shouldBe` ["Player A is not in the game."]
             g' `shouldBe` newGame
 
@@ -71,15 +91,15 @@ spec = describe "session" $ do
         it "should stop the simulation for a player" $ do
             let (g,_) = doCommand newGame $ Just $ Add A 
             let (g',msg)= doCommand g $ Just $ Start A
-            let (g'',msg)= doCommand g' $ Just $ Stop A
-            msg `shouldBe` ["Stopping simulation for A"]
+            let (g'',msg)= doCommand g' $ Just $ Break A
+            msg `shouldBe` ["Pausing simulation for A"]
             let (_,msg)= doCommand g'' $ Just List
             msg  `shouldBe` ["A: Idle Temp=15.0 Pos=100"]
 
         it "should start the simulation for all players" $ do
             let (g',_) = doCommand newGame $ Just $ Add A
             let (g'',_)= doCommand g' $ Just $ Add B
-            let (g''',msg)=doCommand g'' $ Just Go
+            let (g''',msg)=doCommand g'' $ Just Run
             msg `shouldBe` ["Starting all simulations"]
             
             let (_,msg)= doCommand g''' $ Just List
@@ -90,9 +110,9 @@ spec = describe "session" $ do
         it "should stop the simulation for all players" $ do
             let (g',_) = doCommand newGame $ Just $ Add A
             let (g'',_)= doCommand g' $ Just $ Add B
-            let (g''',_)=doCommand g'' $ Just Go
-            let (g'''',msg)=doCommand g''' $ Just Halt
-            msg `shouldBe` ["Stopping all simulations"]
+            let (g''',_)=doCommand g'' $ Just Run
+            let (g'''',msg)=doCommand g''' $ Just End
+            msg `shouldBe` ["Pausing all simulations"]
             
             let (_,msg)= doCommand g'''' $ Just List
             msg  `shouldBe` ["A: Idle Temp=15.0 Pos=100"
